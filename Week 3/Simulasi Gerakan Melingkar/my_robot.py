18c900632daa5796f7b409b44f717acd52bd5d62
Irman Prayista <140878493+irmanprayista31@users.noreply.github.com>
2024-10-12 16:27:28 +0700

    Create my_robot.py

---
+++ Week 3/Simulasi Gerakan Melingkar/my_robot.py
@@ -0,0 +1,25 @@
+from controller import Robot
+
+TIME_STEP = 64 # waktu untuk menjalankan satu langkah simulasi
+MAX_SPEED = 6.28 # set kecepatan maksimal robot
+robot = Robot()
+
+# motor roda kiri dan kanan
+leftMotor = robot.getDevice("left wheel motor")
+rightMotor = robot.getDevice("right wheel motor")
+
+# mengatur posisi motor agar berjalan tanpa batas (mode kecepatan)
+leftMotor.setPosition(float("inf"))
+rightMotor.setPosition(float("inf"))
+
+# mengatur kecepatan awal = 0.0
+leftMotor.setVelocity(0.0)
+rightMotor.setVelocity(0.0)
+
+timestep = int(robot.getBasicTimeStep())
+
+# Loop untuk menggerakkan robot
+while robot.step(TIME_STEP) != -1:
+    # Menggerakkan robot maju dengan mengatur kecepatan pada kedua roda
+    leftMotor.setVelocity(0.5*MAX_SPEED)
+    rightMotor.setVelocity(MAX_SPEED)
